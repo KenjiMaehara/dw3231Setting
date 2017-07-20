9deb9f41b414c71ba086fb5081093b72cf317879
Kenji <kmaehara@mel.co.jp>
2017-07-20 17:31:29 +0900

    再修正

--- sub03.py
+++ sub03.py
@@ -1,26 +1,27 @@
 import paho.mqtt.client as mqtt
 
-host = '127.0.0.1'
+host = '192.168.1.54'
 port = 1883
 topic = 'iktakahiro/a'
 
 def on_connect(client, userdata, flags, respons_code):
-    print('status {0}'.format(respons_code))
+	print('status {0}'.format(respons_code))
 
-    client.subscribe(topic)
+	client.subscribe(topic)
 
 def on_message(client, userdata, msg):
-    print(msg.topic + ' ' + str(msg.payload))
+	print(msg.topic + ' ' + str(msg.payload))
 
 if __name__ == '__main__':
 
-    # Publisherと同様に v3.1.1を利用
-    client = mqtt.Client(protocol=mqtt.MQTTv311)
+	# Publisherと同様に v3.1.1を利用
+	client = mqtt.Client(protocol=mqtt.MQTTv311)
 
-    client.on_connect = on_connect
-    client.on_message = on_message
+	client.on_connect = on_connect
+	client.on_message = on_message
 
-    client.connect(host, port=port, keepalive=60)
+	client.connect(host, port=port, keepalive=60)
 
-    # 待ち受け状態にする
-    client.loop_forever()
+	# 待ち受け状態にする
+	client.loop_forever()
+
