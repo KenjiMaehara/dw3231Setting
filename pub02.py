79d40a70d7d83ffbe3f89a9cc75f60a19c9bb4d9
KenjiMaehara <kmaehara@mel.co.jp>
2017-07-21 14:51:36 +0900

    新しいパブリッシャーを追加。

---
+++ pub02.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+import paho.mqtt.client as mqtt
+ 
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.publish("isyjp/msg1st", "Hello MQTT.", 0)
+ 
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+ 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.tls_set("/etc/ssl/certs/ca-certificates.crt")
+ 
+client.username_pw_set("cloudMQTTのユーザ名", "パスワード")
+client.connect("cloudMQTTのURL", SSLポート番号)
+ 
+client.loop_forever()
